312c7ddd0145ff10e3249563e9ee1267fb7f0f6a
CsignS <smileroom2010@gmail.com>
2021-10-19 19:08:29 +0700

    Initial commit

---
+++ src/main/java/com/example/springsecuritydemo1/SecurityConfiguration.java
@@ -0,0 +1,53 @@
+package com.example.springsecuritydemo1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //set your configuration on the auth object
+        auth
+                .inMemoryAuthentication()
+                .withUser("blah")
+                .password("blah")
+                .roles("USER")
+                .and()
+                .withUser("foo")
+                .password("foo")
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+//                .antMatchers("/","static/css","static/js").permitAll()//this for run css and js file
+
+                //admin
+                .antMatchers("/admin")
+                //.hasRole("USER")=> all user
+//                .hasAnyRole("ADMIN")
+                .hasRole("ADMIN")
+
+                //user
+                .antMatchers("/user")
+                .hasAnyRole("USER","ADMIN")
+
+                //all
+                .antMatchers("/")
+                .permitAll()
+                .and().formLogin();
+    }
+}
